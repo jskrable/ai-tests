dcd8a4ae16f9e0a763768d2514961a93b1bd8369
Jack Skrable <jskrable@bu.edu>
2018-09-19 14:23:10 -0400

    initial commit v1.0

---
+++ nq.py
@@ -0,0 +1,56 @@
+# nq.py
+# author: Jack Skrable
+# date: 09/19/2018
+# desc: solves the n-queens problem recursively 
+
+import sys
+import cProfile
+def safe(board,col):
+	# loop thru previous columns
+	for i in range(col):
+		# attacked if in the same row
+		if (board[i]==board[col]):
+			return False
+		# attacked if on a diag
+		if (col-i==abs(board[col]-board[i])):
+			return False
+	# safe otherwise
+	return True
+
+def solve(board,col,n):
+	# end if past the last column
+	if (col == n): 
+		return True
+	else:
+		# loop thru rows
+		for i in range(n):
+			# place queen
+			board[col]=i
+			if safe(board,col):
+				# if safe, try the next column
+				solved=solve(board,col+1,n)
+				# if we hit the end, all are safe
+				if solved:
+					return True
+		return False
+
+def display(solution):
+	for row in solution:
+		print(row)
+
+def main():
+
+	n = int(sys.argv[1])
+
+	N = n
+
+	board = [-1]*n
+	solution = [[0]*n for i in range(n)]
+	solve(board,0,n)
+
+	print(board)
+
+	#display(board)
+
+if __name__ == "__main__":
+	cProfile.run('main()')
