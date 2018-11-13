b0d351c002aab87b8297dff6d1ea2654e85596a3
Jack Skrable <jskrable@bu.edu>
2018-11-13 13:17:18 -0500

    blocks v0.3 classes

--- blocks.py
+++ blocks.py
@@ -3,20 +3,23 @@ n = 3
 table = [[]]*n
 
 # start position of blocks
-start = [[3],[1,2],[]]
+start = [['B'],['C','A'],[]]
+
+def clearTop
 
 # check for win conditions
 def goal(table):
-	# get winning stack
-	stack = []
-	for i in range(len(table)):
-		stack.append(i+1)
+	# winning stack
+	stack = ['A','B','C']
 	# loop thru table check for win
 	for i in range(len(table)):
 		if table[i] == stack:
 			return True
 	# return false if no win stack
 	return False
+
+def precond(table,block,dest):
+	if table[src] 
 
 # perform move of block
 def move(table,src,dest):
@@ -26,3 +29,57 @@ def move(table,src,dest):
 	del table[src][0]
 	return table
 
+class Block:
+
+	def __init__(self,id,on):
+		self.id = id
+		self.on = on
+
+
+	def clear(self):
+		for i in node:
+			if node[i].on != 'table':
+				node[i].on.clear = False
+		
+		self.clear = True
+
+	def move(self,dest):
+
+		if dest == 'table':
+			self.clear()
+			if self.clear:
+				self.on = 'table'
+		else:
+			self.clear()
+			dest.clear()
+			if self.clear and dest.clear:
+				self.on = dest
+				dest.clear = False
+				return True
+			else:
+				return False
+
+
+
+class State:
+
+	def __init__(self,a,b,c):
+		self.goal = False
+		self.subgoal = False
+
+		if a.on == b and b.on == c and c.on == 'table':
+			self.goal = True
+		elif a.on == b or b.on == c or c.on == 'table':
+			self.subgoal = True
+		
+
+class Node:
+
+	def __init__(self,depth,state):
+		self.depth = depth
+
+
+
+class Stack:
+
+	def __init__(self,)
